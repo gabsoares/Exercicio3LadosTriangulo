3d43eccd3d409332b6826c553bc7ac30f2ab0313
gabsoares <soaresgabriel68@gmail.com>
2024-04-22 09:06:55 -0300

    Remoção de variáveis inutilizáveis

--- Exercicio3LadosTriangulo/Program.cs
+++ Exercicio3LadosTriangulo/Program.cs
@@ -1,4 +1,4 @@
-﻿double lado1, lado2, lado3, lado1Quadrado, lado2Quadrado, lado3Quadrado;
+﻿double lado1, lado2, lado3;
 
 Console.Write("Digite o primeiro valor do triângulo: ");
 lado1 = double.Parse(Console.ReadLine());
@@ -6,10 +6,6 @@ Console.Write("Digite o segundo valor do triângulo: ");
 lado2 = double.Parse(Console.ReadLine());
 Console.Write("Digite o terceiro valor do triângulo: ");
 lado3 = double.Parse(Console.ReadLine());
-
-lado1Quadrado = lado1 * lado1;
-lado2Quadrado = lado2 * lado2;
-lado3Quadrado = lado3 * lado3;
 
 if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1)
 {
